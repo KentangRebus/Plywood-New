69dbc9cd13d904283be2732d9aae6dc3a63b8b8d
KentangRebus <steven.ferdianto0110@gmail.com>
2020-07-12 20:48:50 +0700

    staff migration
    user equal to staff

--- app/User.php
+++ app/User.php
@@ -2,13 +2,17 @@
 
 namespace App;
 
+use App\Traits\UuidTrait;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use UuidTrait;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
