079a9420aeaed9ce2b57ad10b47b156595b88407
KentangRebus <steven.ferdianto0110@gmail.com>
2020-07-13 23:27:22 +0700

    complete the dashboard

--- resources/views/nav/sidenav.blade.php
+++ resources/views/nav/sidenav.blade.php
@@ -2,10 +2,24 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
         <li class="nav-item">
-            <a class="nav-link" href="index.html">
+            <a class="nav-link" href="{{route('home')}}">
                 <span class="menu-title">Dashboard</span>
                 <i class="mdi mdi-home menu-icon"></i>
             </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#general-pages" aria-expanded="false" aria-controls="general-pages">
+                <span class="menu-title">Manage Transaction</span>
+                <i class="menu-arrow"></i>
+                <i class="mdi mdi-medical-bag menu-icon"></i>
+            </a>
+            <div class="collapse" id="general-pages">
+                <ul class="nav flex-column sub-menu">
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/blank-page.html">Add Transaction</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/blank-page.html">Update Transaction</a></li>
+                    <li class="nav-item"> <a class="nav-link" href="pages/samples/blank-page.html">Delete Transaction</a></li>
+                </ul>
+            </div>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="pages/icons/mdi.html">
